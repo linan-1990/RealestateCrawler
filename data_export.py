c7f5ba575027f56d8a208dfed85fd08461bf95b3
Nan Li <linan.lqq0@gmail.com>
2023-04-08 21:44:25 +1000

    update export

--- data_export.py
+++ data_export.py
@@ -28,25 +28,25 @@ except mysql.connector.Error as err:
     exit()
 
 def export_excel(state): 
-    sql_cmd = """ SELECT address, suburb, postcode, house_type, bedroom, bathroom, parking, sold_price, sold_date FROM {}
-        WHERE sold_date is not null and sold_price is not null AND YEAR(sold_date) > 2017
+    sql_cmd = """ SELECT address, suburb, postcode, house_type, land_size, bedroom, bathroom, parking, sold_price, sold_date, link FROM {}
+        WHERE sold_date is not null and sold_price is not null AND YEAR(sold_date) > 2020
         AND sold_price < 100000000 and sold_price > 10000
         ORDER BY YEAR(sold_date)""".format(state)
     df = sql.read_sql(sql_cmd, con)
     df.to_excel(writer, index=False, sheet_name = state)
-    print("Successfully created excel file")
 
 file_name = os.getcwd()
 file_name = file_name + "/Results/data.xlsx";    
 writer = pd.ExcelWriter(file_name, engine = 'openpyxl')
-export_excel('australian_capital_territory')
-export_excel('new_south_wales')
-export_excel('northern_territory')
-export_excel('queensland')
-export_excel('south_australia')
-export_excel('tasmania')
+#export_excel('australian_capital_territory')
+#export_excel('new_south_wales')
+#export_excel('northern_territory')
+#export_excel('queensland')
+#export_excel('south_australia')
+#export_excel('tasmania')
 export_excel('victoria')
-export_excel('western_australia')
+#export_excel('western_australia')
 writer.save()
 writer.close()
 con.close()
+print("Export Successfully!")
